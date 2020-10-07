f4c96aed632c8fea7c3b5c9d95998407a543b3b8
Jacqui Lough <lough.jacqui@gmail.com>
2020-10-07 12:43:21 +1100

    working version of full article model

--- api/tinynews-models/src/plugins/models/article.model.ts
+++ api/tinynews-models/src/plugins/models/article.model.ts
@@ -1,8 +1,9 @@
 // @ts-ignore
 // import { withFields, withName, string, datetime, boolean, ref } from "@webiny/commodo";
-import { withFields, withName, string } from "@webiny/commodo";
+import { withFields, withName, string, boolean, ref } from "@webiny/commodo";
+import { date } from "commodo-fields-date";
 import { flow } from "lodash";
-// import { i18nString } from "@webiny/api-i18n/fields";
+import { i18nString } from "@webiny/api-i18n/fields";
 import { Context as APIContext } from "@webiny/graphql/types";
 import { Context as I18NContext } from "@webiny/api-i18n/types";
 import { Context as CommodoContext } from "@webiny/api-plugin-commodo-db-proxy/types";
@@ -16,32 +17,32 @@ export default ({ context, createBase }: Article) => {
     return flow(
         withName("Article"),
         withFields(() => ({
-            // headline: i18nString({ context }),
-            // content: i18nString({ context }),
+            headline: i18nString({ context }),
+            content: i18nString({ context }),
             slug: string(),
-            // authorSlugs: string(),
-            // customByline: string(),
-            // searchTitle: i18nString({ context }),
-            // searchDescription: i18nString({ context }),
-            // twitterTitle: i18nString({ context }),
-            // twitterDescription: i18nString({ context }),
-            // facebookTitle: i18nString({ context }),
-            // facebookDescription: i18nString({ context }),
-            // firstPublishedOn: datetime(),
-            // lastPublishedOn: datetime(),
-            // published: boolean({ value: false }),
-            // authors: ref({
-            //     list: true,
-            //     instanceOf: context.models.Author
-            // }),
-            // category: ref({
-            //     list: false,
-            //     instanceOf: context.models.Category
-            // }),
-            // tags: ref({
-            //     list: true,
-            //     instanceOf: context.models.Tag
-            // }),
+            authorSlugs: string(),
+            customByline: string(),
+            searchTitle: i18nString({ context }),
+            searchDescription: i18nString({ context }),
+            twitterTitle: i18nString({ context }),
+            twitterDescription: i18nString({ context }),
+            facebookTitle: i18nString({ context }),
+            facebookDescription: i18nString({ context }),
+            firstPublishedOn: date(),
+            lastPublishedOn: date(),
+            published: boolean({ value: false }),
+            authors: ref({
+                list: true,
+                instanceOf: context.models.Author
+            }),
+            category: ref({
+                list: false,
+                instanceOf: context.models.Category
+            }),
+            tags: ref({
+                list: true,
+                instanceOf: context.models.Tag
+            })
         }))
     )(createBase());
 };
